0de66c4b1cabeea6d46e591a62c63a26c0e34527
jeshurunmuchugingugi <jeshurun.muchugi@student.moringaschool.com>
2025-06-12 15:32:52 +0300

    scope lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+
+
+// Declare customerName in the global scope explicitly
+var customerName = 'bob';
+
+// Modify the global customerName
+function upperCaseCustomerName() {
+  window.customerName = window.customerName.toUpperCase();
+}
+
+
+function setBestCustomer(){
+  bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+}
+const leastFavoriteCustomer = 'kamau';
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'njogu';
+}
